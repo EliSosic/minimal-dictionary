e3f87b1463aee87cad7f4d515f1b76befb017c1f
Elisa <elisosic@gmail.com>
2024-03-27 20:00:42 +0100

    updated header

---
+++ src/Dictionary.js
@@ -0,0 +1,17 @@
+import React, { useState }from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    function search (event) {
+        event.preventDefault ();
+        alert("searching");
+    }
+
+    return (
+    <div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" />
+        </form>
+    </div>
+    ); 
+}
