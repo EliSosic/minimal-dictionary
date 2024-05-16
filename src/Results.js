270b9e340edaab5c27f1a0705522b46b0882b327
Elisa <elisosic@gmail.com>
2024-05-16 22:10:43 +0200

    inserted resaults.js

---
+++ src/Results.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Results(props) {
+    if (props.results) {
+        return (
+            <div>
+                <h2> {props.results.word} </h2>
+                
+            </div>
+        )
+
+    } else {
+        return null;
+    }
+
+}
