99bd37b4f26fb24ff2ba6d6f7ec3f6e0fa8294da
Elisa <elisosic@gmail.com>
2024-05-25 20:07:34 +0200

    added css to photos and adjusted responsive components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -48,7 +48,9 @@ export default function Dictionary() {
             </form>
         </div>
         <Results  results={results} />
-        <Photos photos={photos} />
+        <Photos photos={photos} /> 
     </div>
+ 
+
     ); 
 }
--- src/Photos.js
+++ src/Photos.js
@@ -3,15 +3,14 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos (props) {
-    console.log(props.photos);
     if (props.photos) {
         return (
             <div className="Photos">
                 <div className="row">
-                    {props.photos.map(function (photos, index) { 
+                    {props.photos.map(function (photo, index) { 
                         return (
-                            <div className="col-3" key={index}>
-                             <img src={photos.src.landscape} className="img-fluid" alt="" />
+                            <div className="col-4" key={index}>
+                             <img src={photo.src.landscape} className="img-fluid" alt="" />
                             </div>
                     );
                 })}
